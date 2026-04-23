b9290cfe8374a7aed8afaae7c28ebf8e7ae8654f
Arepa_Chocolo <Arepa_Chocolo@DESKTOP-4M1GI5M>
2026-04-23 14:25:17 -0500

    Implementacion del Patron Proxy

--- flotasytransportes/src/main/java/com/flotasytransportes/infraestructura/config/ConfiguracionRutas.java
+++ flotasytransportes/src/main/java/com/flotasytransportes/infraestructura/config/ConfiguracionRutas.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 import com.flotasytransportes.aplicacion.rutas.RutaRapidaService;
 import com.flotasytransportes.aplicacion.rutas.ServicioRutas;
+import com.flotasytransportes.aplicacion.servicio.CalculadoraDistanciaService;
+import com.flotasytransportes.dominio.puerto.proxy.CacheAlmacenamiento;
+import com.flotasytransportes.dominio.puerto.proxy.CacheEnMemoria;
+import com.flotasytransportes.dominio.puerto.proxy.DistanciaServiceProxy;
+import com.flotasytransportes.dominio.puertos.DistanciaServicePort;
+import com.flotasytransportes.infraestructura.integracion.DistanciaAdapter;
 import com.flotasytransportes.infraestructura.navegacion.NavegacionAPI;
 
 @Configuration
@@ -16,5 +22,28 @@ public class ConfiguracionRutas {
             @Qualifier("googleMapsAPI") NavegacionAPI api) {
 
         return new RutaRapidaService(api);
+    }
+
+    @Bean
+    public CalculadoraDistanciaService calculadoraDistanciaService() {
+        return new CalculadoraDistanciaService();
+    }
+
+    @Bean
+    public DistanciaAdapter distanciaAdapter(
+            CalculadoraDistanciaService calculadora) {
+        return new DistanciaAdapter(calculadora);
+    }
+
+    @Bean
+    public CacheAlmacenamiento<Double> cacheDistancias() {
+        return new CacheEnMemoria<>(100);
+    }
+
+    @Bean
+    public DistanciaServicePort distanciaServicePort(
+            DistanciaAdapter adapterReal,
+            CacheAlmacenamiento<Double> cache) {
+        return new DistanciaServiceProxy(adapterReal, cache);
     }
 }
